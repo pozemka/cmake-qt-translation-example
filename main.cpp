2123fe7465774acd925f43d4dfa31ee5f3fd042f
Mikhail <push.uni@gmail.com>
2021-09-15 12:36:15 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = QLocale(locale).name();
+        if (translator.load(":/lang/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    QString test = QObject::tr("Hello!");
+
+    qDebug("%s", qPrintable(test));
+
+    return a.exec();
+}
